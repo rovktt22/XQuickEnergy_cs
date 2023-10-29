91f7b1ba0ffb53f204b75616e3ee67add87e09c8
rovktt22 <141018852+rovktt22@users.noreply.github.com>
2023-10-30 00:15:37 +0800

    Add files via upload

--- app/src/main/java/pansong291/xposed/quickenergy/AntForest.java
+++ app/src/main/java/pansong291/xposed/quickenergy/AntForest.java
@@ -381,6 +381,7 @@ public class AntForest {
                         if(jaBubbles.length()<>1)
                         {int ccc=jaBubbles.length()-1;
                         for(int q=1;q<ccc;q++){
+                        q=q+1;
                         JSONObject bubble2 = jaBubbles.getJSONObject(q);
                         long bubbleId2 = bubble2.getLong("id");
                         String bbb2=Long.toString(bubbleId2);
@@ -558,6 +559,7 @@ public class AntForest {
                     int ccc=jaBubbles.length()-1;
                     if(jaBubbles.length()<>1)
                     {for(int q=1;q<ccc;q++){
+                        q=q+1;
                         //int iti=q;
                     JSONObject bubble2 = jaBubbles.getJSONObject(q);
                     long bubbleId2 = bubble2.getLong("id");
