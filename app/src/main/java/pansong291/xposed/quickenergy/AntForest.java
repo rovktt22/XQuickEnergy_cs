4b17281fccf41d232dd8634e502309247faf833d
rovktt22 <141018852+rovktt22@users.noreply.github.com>
2023-11-01 00:02:37 +0800

    Add files via upload

--- app/src/main/java/pansong291/xposed/quickenergy/AntForest.java
+++ app/src/main/java/pansong291/xposed/quickenergy/AntForest.java
@@ -387,7 +387,7 @@ public class AntForest {
                         //addid.append(","+bbb2);
                         int bvid=q+1;
                         //ztck.append(CollectStatus.valueOf(bubble2.getString("collectStatus")));
-                        if(CollectStatus.valueOf(bubble2.getString("collectStatus"))=="AVAILABLE")
+                        if(CollectStatus.valueOf(bubble2.getString("collectStatus"))==AVAILABLE)
                         {if(jaBubbles.length()==bvid)
                         {addid.append(bbb2);}
                         else
@@ -574,7 +574,7 @@ public class AntForest {
                     String bbb2=Long.toString(bubbleId2);
                     //ztck.append(CollectStatus.valueOf(bubble2.getString("collectStatus")));
                     int bpid=q+1;
-                    if(CollectStatus.valueOf(bubble2.getString("collectStatus"))=="AVAILABLE")
+                    if(CollectStatus.valueOf(bubble2.getString("collectStatus"))==AVAILABLE)
                     {if(jaBubbles.length()==bpid)
                     {addid.append(bbb2);}
                     else
