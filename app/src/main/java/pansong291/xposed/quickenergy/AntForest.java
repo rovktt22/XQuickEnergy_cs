d8010ea9ea05dca1ea0bf8742c48c28b40400261
rovktt22 <141018852+rovktt22@users.noreply.github.com>
2023-11-03 00:38:06 +0800

    Add files via upload

--- app/src/main/java/pansong291/xposed/quickenergy/AntForest.java
+++ app/src/main/java/pansong291/xposed/quickenergy/AntForest.java
@@ -855,7 +855,8 @@ private static int collectEnergy2(String userId, String bubbleId, String bizNo, 
                 JSONArray jaBubbles = jo.getJSONArray("bubbles");
                 jo = jaBubbles.getJSONObject(0);
                 for (int i = 0; i <ssttp; i++) {
-                JSONArray jaBubbles2 = jo.getJSONArray("bubbles");
+                JSONObject jo2 = new JSONObject(s);
+                JSONArray jaBubbles2 = jo2.getJSONArray("bubbles");
                 jo2 = jaBubbles2.getJSONObject(i);
                 collected += jo2.getInt("collectedEnergy");
                 }
